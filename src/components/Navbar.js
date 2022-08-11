257cfd7d0be9fd6a8e81a6a24e0d4f8947d82953
bhinder <bhinderrr@gmail.com>
2022-08-11 15:12:39 -0400

    navbar categories + buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom'
 import "./Navbar.css"
 
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-    <div className='navbar'>
-      <a className='logo'>
-        GuruLangar
-      </a>
-    </div>
+    <>
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <Link to="/" className="navbar-logo">
+          GuruLangar
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? 'nav-menu active' : "nav-menu"}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/about-us' className='nav-links' onClick={closeMobileMenu}>
+              About Us
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </>
   )
 }
 
