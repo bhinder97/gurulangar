71dff7025ac5e21b9f7a86d44b2d707bc006001a
bhinder <bhinderrr@gmail.com>
2023-05-30 16:49:26 -0400

    trying to fix video playback

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 import '../App.css'
 import { Button } from './Button'
 import './About.css'
 
+const url = "/home/labber/lighthouse/portfolio/gurulangar/public/videos/testvideo.mp4"
 
 function About() {
   return (
     <div className='about-container'>
-        {/* <video controls autoPlay loop muted>
-          <source src='/home/labber/lighthouse/portfolio/gurulangar/public/videos/video-2.mp4' type='video/mp4'></source>
-        </video> */}
+        <video muted controls autoPlay={"autoplay"} loop>
+          <source src='public/videos/testvideo.mp4' type='video/mp4'></source>
+        </video>
+        {/* <video src='public/videos/video-1.mp4' autoPlay loop muted /> */}
+        {/* <ReactPlayer url={url} muted loop playing /> */}
+        {/* <VideoPlayer /> */}
         
         <h1>Welcome</h1>
         <p>Gurulangar is a non-profit organizationt that helps Sikhs in need within punjab</p>
@@ -26,7 +31,7 @@ function About() {
             buttonStyle='btn--primary' 
             buttonSize='btn--large'
             >
-              Watch Trailer <i className='far fa-play-circle' />
+              YouTube <i className='far fa-play-circle' />
             </Button>
         </div>
     </div>
